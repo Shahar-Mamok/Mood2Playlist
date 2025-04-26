99dc8faf1d77021764b7ed2c3ac9fb5c04eb9cca
Shahar=Mamok <Shaharmamok7@outlook.com>
2025-04-27 00:28:12 +0300

    First commit - Basic project structure

---
+++ src/controller/PlaylistController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import view.MainView;
+
+public class PlaylistController {
+
+    private MainView view;
+
+    public PlaylistController(MainView view) {
+        this.view = view;
+        initListeners();
+    }
+
+    private void initListeners() {
+        // Connect the button in the future
+    }
+}
---
+++ src/view/MainView.java
@@ -0,0 +1,42 @@
+package view;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.util.PrimitiveIterator;
+
+public class MainView extends Application {
+    private TextField MoodInput;
+    private Button GentatePlaylist;
+    private TextArea resultArea;
+
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Start Vibing");
+
+        MoodInput = new TextField();
+        GentatePlaylist = new Button("Gentate Playlist");
+        resultArea = new TextArea();
+        resultArea.setEditable(false);
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, MoodInput, GentatePlaylist, resultArea);
+
+        Scene scene = new Scene(layout, 400, 400);
+        primaryStage.setTitle("Mood2Playlist");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
